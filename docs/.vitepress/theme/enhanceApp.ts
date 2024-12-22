0a6910993efc98137c45c2f72e6df563c0488c0b
Alowree Xu <alowree@hotmail.com>
2024-12-22 22:31:59 +0800

    Created Posts.vue but still need to debug the post list on index.md

--- docs/.vitepress/theme/enhanceApp.ts
+++ docs/.vitepress/theme/enhanceApp.ts
@@ -2,8 +2,10 @@ import TwoslashFloatingVue from "@shikijs/vitepress-twoslash/client";
 import vitepressBackToTop from "vitepress-plugin-back-to-top";
 import { enhanceAppWithTabs } from "vitepress-plugin-tabs/client";
 import SvgImage from "../components/SvgImage.vue";
+import Posts from "../components/Posts.vue";
 export default ({ app, router, siteData }) => {
   app.component("SvgImage", SvgImage);
+  app.component("Posts", Posts);
   enhanceAppWithTabs(app);
   vitepressBackToTop({ threshold: 300 }); // plugin: vitepress-plugin-back-to-top
   app.use(TwoslashFloatingVue);
